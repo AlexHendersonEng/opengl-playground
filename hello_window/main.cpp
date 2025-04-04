4424f96e7fe63e805cf66ae3202ed81ebd084191
Alex Henderson <alexhendersnao@gmail.com>
2025-04-04 16:57:40 +0100

    Added hello_window project

---
+++ hello_window/main.cpp
@@ -0,0 +1,51 @@
+﻿// Include necessary headers
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <cstdlib>
+
+// Define the framebuffer size callback function
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+};
+
+int main() {
+	// Initialise GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create window and context
+	GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+	if (window == nullptr) {
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialise GLAD
+	if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	// Setup viewport
+	glViewport(0, 0, 800, 600);
+
+	// Set the viewport size callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Cleanup
+	glfwTerminate();
+
+	// Return success
+	return EXIT_SUCCESS;
+}
